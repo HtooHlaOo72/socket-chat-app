cccad9b61a90bcffba83af9324ab328e0cdefc83
HtooHlaOo72 <htoohlaoo@uit.edu.mm>
2021-09-12 10:04:30 +0630

    cant send after id change updated

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app=express()
 
 //server set up 
 const server=app.listen(process.env.PORT||4000,()=>{
-    console.log('server is running on port 4000');
+    //console.log('server is running on port 4000');
 })
 
 app.get('/',(req,res)=>{
@@ -16,12 +16,13 @@ const io=socket(server);
 io.on('connection',(connection)=>{
     console.log(connection.id,' is connected...');
     connection.on('chat',(data)=>{
-        console.log('room_id',data.room)
-        io.sockets.emit(data.room+'',data);
+        io.sockets.emit('chat',data);
+        console.log('room_id',data.room);
+
     })
     connection.on(`typing`,data=>{
-        console.log('typing',data.name);
-        connection.broadcast.emit(`typing${data.room}`,data);
+        //console.log('typing',data.name,data.room);
+        connection.broadcast.emit(`typing`,data);
     })
 
 })
